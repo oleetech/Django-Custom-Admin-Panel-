efeba0d46d2bcc1e90626594f03cdab5e0575d2b
Olee Ahmmed Ashik <124195105+oleetech@users.noreply.github.com>
2024-02-19 01:15:11 +0600

    create admin with tailwingcss config

---
+++ myapp/models.py
@@ -0,0 +1,12 @@
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./templates/**/*.html', './static/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
